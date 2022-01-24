402aae2a603790af3a6ab482a3cb2b3328bdcb10
youngG124 <newyoung124@naver.com>
2022-01-24 15:36:53 +0900

    직접 객체 만들어보기

--- object_oriented_programming/built_in.js
+++ object_oriented_programming/built_in.js
@@ -1,3 +1,17 @@
 console.log(Math.PI);
 console.log(Math.random());
 console.log(Math.floor(3.9));
+
+var MyMath = { // 서로 연관된 변수와 함수를 grouping.
+    PI : Math.PI,
+    random : function(){
+        return Math.random();
+    },
+    floor : function(val){
+        return Math.floor(val);
+    }
+}
+
+console.log(MyMath.PI);
+console.log(MyMath.random());
+console.log(MyMath.floor(4.2));
