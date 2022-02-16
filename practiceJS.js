ff0c618593b414587164d8744fb623d07007da6d
youngG124 <newyoung124@naver.com>
2022-02-17 00:36:11 +0900

    practice

---
+++ practiceJS.js
@@ -0,0 +1,17 @@
+let name = "Kim";
+const PI = 3.14;
+
+// let name = "Lee"; 오류
+
+name = "Lee";
+console.log(name);
+
+name = "Seo";
+
+const message = (`My name is ${name}`);
+console.log(message);
+
+// null은 존재하지 않는 값
+// undefined는 할당되지 않은 값
+ 
+
