0939f00dabb13e571a5c1d6ca5bfb286c74b4df6
youngG124 <newyoung124@naver.com>
2022-02-24 23:59:18 +0900

    class prtc

--- javascript_mid/class.js
+++ javascript_mid/class.js
@@ -8,6 +8,7 @@ const User = function (name, age) {
 
 const mike = new User("Mike", 30);
 
+//ES6에 추가된 class
 class User2 {
     constructor(name, age) {
         this.name = name;
@@ -19,8 +20,32 @@ class User2 {
 }
 
 const tom = new User2("Tom", 19);
+//class는 new없이 실행하면 type Error가 뜸!!
 
 console.log(mike);
 console.log(tom);
 
 mike.showName();
+
+class Car {
+    constructor(color) {
+        this.color = color;
+        this.wheels = 4;
+    }
+    drive() {
+        console.log("drive...");
+    }
+    stop() {
+        console.log("STOP");
+    }
+}
+
+class Bmw extends Car {
+    park() {
+        console.log("Park");
+    }
+}
+
+const m3 = new Bmw("Blue");
+
+console.log(m3);
