d767328bd899821cef1caecd064b1c65c89152b2
youngG124 <newyoung124@naver.com>
2022-02-27 21:51:25 +0900

    prtc

--- syntax/ternary_operator.js
+++ syntax/ternary_operator.js
@@ -13,3 +13,32 @@ function print(a) {
 var num = 5;
 num **= 2;
 print(num);
+
+console.log('-----------');
+
+var a = 'cat'||'dog';
+var b = false&&'dog';
+
+console.log(a);
+console.log(b);
+
+console.log('-----------');
+//단축 평가로 if문 대체
+var done = true;
+var message = '';
+
+message = done && '완료';
+console.log(message);
+
+
+var done2 = false;
+var message = '';
+
+message2 = done2 || '미완료';
+console.log(message2);
+
+var done3 = true;
+var message = '';
+
+message = done ? '완료' : '미완료'; // if(done) message = '완료'; else message = '미완료';
+console.log(message);
