0442cb2047af49f9e3478169c5259d808f2cfeaf
youngG124 <newyoung124@naver.com>
2022-02-27 22:02:16 +0900

    단축 평가로 default값 설정

--- syntax/ternary_operator.js
+++ syntax/ternary_operator.js
@@ -42,3 +42,13 @@ var message = '';
 
 message = done ? '완료' : '미완료'; // if(done) message = '완료'; else message = '미완료';
 console.log(message);
+
+// 단축 평가를 사용한 매개변수의 기본값 설정
+function getStringLength(str) {
+    str = str || 'kkkk';
+    return str.length;
+}
+// 매개변수가 전달되지 않을 경우 단축 평가를 사용해 default값을 정할 수 있음. str = ''로.
+
+// 인자로 아무것도 넘기지 않았지만, 단축 평가를 사용해서 kkkk를 기본값으로 설정하여 함수의 결과가 4가 된다.
+console.log(getStringLength());
