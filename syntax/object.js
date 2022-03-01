d74e46f1967ebf26f3cf751eebb52149081cb7a4
youngG124 <newyoung124@naver.com>
2022-03-01 22:40:36 +0900

    prtc

--- syntax/object.js
+++ syntax/object.js
@@ -50,3 +50,31 @@ const obj2 = {
 };
 
 console.log(obj2);
+
+const o = {};
+o.a = 1;
+console.log(o);
+
+console.log(score); // undefined
+score = 80;
+var score; // 선언은 런타임 이전에 실행된다.
+console.log(score); // 80
+
+// 유사 배열 객체(array-like object)
+// 인덱스를 사용해 각 문자 접근 가능, length 프로퍼티 가짐, for문으로 순회 가능
+var str = 'string';
+console.log(typeof(str));
+
+console.log(str[0]); // 원시 값을 객체처럼 사용하면 "래퍼 객체(21.3절)"로 자동 변환된다.
+
+str[0] = 'S'; // 문자열은 원시 값이므로 변경 불가능. read-only, immutable, 에러는 발생하지 않음. 데이터 신뢰성 보장.
+str = 'S';
+console.log(str); // 하지만 새로운 문자열을 할당하는 것은 가능함.
+
+var score = 80;
+var copy = score;
+
+console.log(copy);
+score = 100;
+console.log(score);
+console.log(copy);
